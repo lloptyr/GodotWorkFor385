e5bead7d9fbdcd1b99d74d8beb062c6d47d105e6
DJ Standish <dustin.standish@yahoo.com>
2023-11-15 19:10:37 -0800

    minor changes

--- Prototype/Source/objects/wallclingrune.gd
+++ Prototype/Source/objects/wallclingrune.gd
@@ -1,7 +1,11 @@
 extends Area2D
 @onready var anim_player: AnimationPlayer=get_node("AnimationPlayer")
 func _on_body_entered(body):
-	PlayerData.set_wallcling(true)
-	anim_player.play("fade_out")
-	await(anim_player.animation_finished)
-	anim_player.play("tutorial")
+	if(self.visible):
+		print("wallcling")
+		PlayerData.set_wallcling(true)
+		anim_player.play("fade_out")
+		await(anim_player.animation_finished)
+		anim_player.play("tutorial")
+		await(anim_player.animation_finished)
+		queue_free()
